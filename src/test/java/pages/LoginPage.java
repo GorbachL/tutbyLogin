cd4deca4cfb72047e36f217e74682db1cc2a7a0c
GorbachL <52042279+GorbachL@users.noreply.github.com>
2020-03-27 20:24:42 +0300

    Updated locators/ Using different types

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -7,11 +7,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends BasePage {
 
-    private static final By ENTER_LINK = By.cssSelector(".enter");
+    private static final By ENTER_LINK = By.className("enter");
     private static final By LOGIN_INPUT = By.xpath("//input[@name='login']");
-    private static final By PASSWORD_INPUT = By.xpath("//input[@name='password']");
+    private static final By PASSWORD_INPUT = By.name("password");
     private static final By LOGIN_BUTTON = By.cssSelector(".button.auth__enter");
     private static final By LOGIN_USER_NAME = By.cssSelector(".enter.logedin .uname");
+    private static final By REMEMBER_CHECKBOX = By.id("memory");
+    private static final By REGISTRATION_LINK = By.linkText("Регистрация");
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -33,8 +35,12 @@ public class LoginPage extends BasePage {
 
     public void loginUsingCorrectCredentials(String loginName, String password) {
         driver.findElement(ENTER_LINK).click();
+        driver.findElement(REGISTRATION_LINK).click();
+        driver.navigate().back();
+        driver.findElement(ENTER_LINK).click();
         driver.findElement(LOGIN_INPUT).sendKeys(loginName);
         driver.findElement(PASSWORD_INPUT).sendKeys(password);
+        driver.findElement((REMEMBER_CHECKBOX)).isSelected();
         driver.findElement(LOGIN_BUTTON).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(LOGIN_USER_NAME));
     }
