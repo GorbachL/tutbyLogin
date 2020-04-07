265b4c04ba2f7a48eb379453ded34f5c4982a9e2
GorbachL <52042279+GorbachL@users.noreply.github.com>
2020-04-07 20:38:14 +0300

    Added implicit waiter for WebDriver

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,17 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.PropertyManager;
 
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
 
     WebDriver driver;
-    WebDriverWait wait;
     static PropertyManager prop;
 
     @BeforeEach
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 20);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
